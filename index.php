4815e64a810998111a27075dc84a9e53c5333f4b
Mark Blashki <markblashki1@gmail.com>
2020-01-14 23:55:46 +1100

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Mark Blashki</title>
+  </head>
+  <body>
+    <h1 id="NameHeading">Mark Blashki</h1>
+    <hr>
+    <h2 id="NameDesc">Web Development |Software Solutions | Information Technology Consulting</h2>
+    <br/>
+    <h3>Phone: 0490 193 446</h3>
+    <h3>Email: markblashki1@gmail.com</h3>
+  </body>
+</html>
