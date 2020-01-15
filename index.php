9a0f046bf4fd031c93e3e3925cf53a40e3c549cb
Mark Blashki <markblashki1@gmail.com>
2020-01-15 18:56:36 +1100

    Responsive design started & Consulting Section almost done

--- index.php
+++ index.php
@@ -1,38 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-		<title>Mark Blashki</title>
-		<link href="https://fonts.googleapis.com/css?family=Play&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/indexStyle.css">
-  </head>
-  <body>
-    <div class="navBar shadow2">
-			<div class="logo">
-				<img src="img/logo.png" alt="Mark Blashki Solutions Logo">
-			</div>
-			<nav class="nav">
-				<ul>
-					<li><a href="About">About</a></li>
-					<li><a href="Services">Services</a></li>
-					<li><a href="Portfolio">Portfolio</a></li>
-				</ul>
-			</nav>
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<title>Mark Blashki</title>
+	<link href="https://fonts.googleapis.com/css?family=Play&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/indexStyle.css">
+	<!--[if IE]>
+		<style type="text/css">
+			/* Stop the image stopping mouse clicks but for IE*/
+			.rightTriangle {
+				filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src="../img/rightTriangle.png", sizingMethod='scale');
+				background: none !important;
+			}
+			</style>
+	<![endif]-->
+</head>
+<body>
+	<div class="navBar shadow2">
+		<div class="logo">
+			<img src="img/logo.png" alt="Mark Blashki Solutions Logo">
 		</div>
-		<div class="hero">
-			<div class="heroContainer">
-				<img src="img/fullLogo.png" alt="Mark Blashki Solutions Logo">
+		<nav class="nav">
+			<ul>
+				<li><a href="About">About</a></li>
+				<li><a href="Services">Services</a></li>
+				<li><a href="Portfolio">Portfolio</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="rightTriangle"></div>
+	<div class="hero">
+		<div class="heroContainer">
+			<img src="img/fullLogo.png" alt="Mark Blashki Solutions Logo">
+			<div class="heroText">
 				<div class="sloganContainer">
-					<h2 class="subheading">Helping Business Through Tecnology.</h2>
-					<h3 class="subtitle">Technology Consulting and Software Solutions</h3>
+					<h3 class="heading">Technology Consulting and Software Solutions</h3>
+					<h2 class="subtitle">Helping Business Through Tecnology.</h2>
 				</div>
 				<div class="buttonContainer">
-					<div class="button shadow1">
+					<a href="Portfolio" class="button shadow1">
 						<h3>See Services</h3>
 						<img src="./img/buttonArrow.png" alt="">
-					</div>
+					</a>
 				</div>
 			</div>
 		</div>
-  </body>
+	</div>
+	<div class="consulting">
+		<div class="consultingContainer">
+			<div class="subheading">
+				<span><img src="img/headingMarker.png" alt=""></span><h1 class="">Consulting</h1>
+			</div>
+			<p class="text">Delivering Quality Advice on Many Different Subjects to Boost Your Business' Performance.</p>
+			<ul>
+				<li>
+					<p class="text">- Integrating Technology with Business</p>
+				</li>
+				<li>
+					<p class="text">- Systems Migration</p>
+				</li>
+				<li>
+					<p class="text">- Technology Implementation</p>
+				</li>
+				<li>
+					<p class="text">- Machine Learning</p>
+				</li>
+				<li>
+					<p class="text">- Systems Restructuring</p>
+				</li>
+			</ul>
+			<a href="Portfolio" class="contactButton button shadow1">
+				<h3>Contact</h3>
+				<img src="./img/buttonArrow.png" alt="">
+			</a>
+		</div>
+	</div>
+</body>
 </html>
