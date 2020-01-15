9c743078a97d297ac3849be7c73a52320970ca7b
Mark Blashki <markblashki1@gmail.com>
2020-01-15 16:41:09 +1100

    Hero Section and Nav Bar complete

--- index.php
+++ index.php
@@ -2,14 +2,37 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Mark Blashki</title>
+		<title>Mark Blashki</title>
+		<link href="https://fonts.googleapis.com/css?family=Play&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/indexStyle.css">
   </head>
   <body>
-    <h1 id="NameHeading">Mark Blashki</h1>
-    <hr>
-    <h2 id="NameDesc">Web Development |Software Solutions | Information Technology Consulting</h2>
-    <br/>
-    <h3>Phone: 0490 193 446</h3>
-    <h3>Email: markblashki1@gmail.com</h3>
+    <div class="navBar shadow2">
+			<div class="logo">
+				<img src="img/logo.png" alt="Mark Blashki Solutions Logo">
+			</div>
+			<nav class="nav">
+				<ul>
+					<li><a href="About">About</a></li>
+					<li><a href="Services">Services</a></li>
+					<li><a href="Portfolio">Portfolio</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="hero">
+			<div class="heroContainer">
+				<img src="img/fullLogo.png" alt="Mark Blashki Solutions Logo">
+				<div class="sloganContainer">
+					<h2 class="subheading">Helping Business Through Tecnology.</h2>
+					<h3 class="subtitle">Technology Consulting and Software Solutions</h3>
+				</div>
+				<div class="buttonContainer">
+					<div class="button shadow1">
+						<h3>See Services</h3>
+						<img src="./img/buttonArrow.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
   </body>
 </html>
