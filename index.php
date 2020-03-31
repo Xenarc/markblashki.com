9fa06ebd3862724f53496c4e79a21f36c5322e46
Mark Blashki <markblashki1@gmail.com>
2020-03-31 14:36:22 +1100

    Added more text to hero and cleaned up css files

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta content="width=device-width, initial-scale=1" name="viewport" />
-	<title>Mark Blashki</title>
+<meta charset="UTF-8">
+  <meta name="description" content="Mark Blashki Solutions offers a range of website design and development services as well as mobile app programming and consulting.">
+  <meta name="keywords" content="Programmer, Developer, mobile, app, IT, solutions, applications, systems, consulting">
+  <meta name="author" content="Mark Blashki">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mark Blashki Solutions | Technology Consulting and Software Solutions</title>
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js"integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
 	<script src="js/index.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Play&display=swap" rel="stylesheet">
@@ -25,141 +28,95 @@
 			<img src="img/logo.png" alt="Mark Blashki Solutions Logo">
 		</div>
 		<div class="hamburger">
-			<img src="img/hamburgerBlue.png" alt="Hamburger Menu">
+			<img src="img/hamburgerBlue.png" alt="Menu">
 		</div>
 		<nav class="nav">
 			<ul>
-				<li><a href="About">About</a></li>
-				<li><a href="Services">Services</a></li>
-				<li><a href="Portfolio">Portfolio</a></li>
+				<li><a href="#consulting">Consulting</a></li>
+				<li><a href="#development">Development</a></li>
+				<li><a href="#portfolio">Portfolio</a></li>
 			</ul>
 		</nav>
-		
 	</div>
 	<div class="rightTriangle"></div>
-	<div class="hero">
+	<section class="hero">
 		<div class="heroContainer container">
 			<img src="img/fullLogo.png" alt="Mark Blashki Solutions Logo">
 			<div class="heroText">
 				<div class="sloganContainer">
-					<h1 class="subheading">Technology Consulting and Software Solutions</h1>
-					<h2 class="subtitle">Helping Business Through Tecnology.</h2>
-				</div>
-				<div class="buttonContainer">
-					<a href="Portfolio" class="button shadow1">
-						<h3>Services</h3>
-						<img src="./img/buttonArrow.png" alt="">
-					</a>
-				</div>
+					<h1 class="subheading">Technology Consulting and Software Solutions</h1><br>
+					<h2 class="subtitle">Website design and development services, mobile app programming, and consulting.</h2>
+					<h4 class="mt1p">Using industry standard techniques and cutting edge technologies, Mark Blashki Solutions is ready to achive tangible results for your business.</h4>
+				<div class="buttonContainer"></div>
 			</div>
 		</div>
-	</div>
-	<div class="consulting">
-		<div class="consultingContainer container">
-			<div class="heading">
-				<span><img src="img/headingMarker.png" alt=""></span><h1 class="">Consulting</h1>
+	</section>
+	<main>
+		<section class="consulting" id="consulting">
+			<div class="consultingContainer container">
+				<div class="heading">
+					<span><img src="img/headingMarker.png" alt="heading"></span><h1 class="">Consulting</h1>
+				</div>
+				<p class="text">Delivering quality advice on many different subjects to boost your business' performance.</p>
+				<ul>
+					<li class="text">Integrating technology with business</li>
+					<li class="text">Systems migration</li>
+					<li class="text">Technology implementation</li>
+					<li class="text">Machine learning</li>
+					<li class="text">Systems restructuring</li>
+				</ul>
+				<a href="#contact" class="button shadow1">
+					<h3>Contact</h3>
+					<img src="./img/buttonArrow.png" alt="Arrow">
+				</a>
 			</div>
-			<p class="text">Delivering quality advice on many different subjects to boost your business' performance.</p>
-			<ul>
-				<li>
-					<p class="text">Integrating technology with business</p>
-				</li>
-				<li>
-					<p class="text">Systems migration</p>
-				</li>
-				<li>
-					<p class="text">Technology implementation</p>
-				</li>
-				<li>
-					<p class="text">Machine learning</p>
-				</li>
-				<li>
-					<p class="text">Systems restructuring</p>
-				</li>
-			</ul>
-			<a href="Portfolio" class="button shadow1">
-				<h3>Contact</h3>
-				<img src="./img/buttonArrow.png" alt="">
-			</a>
-		</div>
-	</div>
-	<div class="softwareSolutions">
-		<div class="softwareSolutionsContainer container">
-			<div class="heading darkTheme">
-				<span><img src="img/headingMarker.png" alt=""></span><h1 class="">Design. Develop. Deploy.</h1>
-			</div>
-			<p class="text darkTheme">Tailored solutions to suit your business. No matter the size.</p>
-			<ul>
-				<li class="customApplications softwareSolutionSections">
-					<h3 class="subtitle darkTheme">Custom Applications</h3>
-					<img src="./img/customApplications.png" alt="Custom Applications">
-					<ul>
-						<li>
-							<p class="text darkTheme">Windows</p>
-						</li>
-						<li>
-							<p class="text darkTheme">Android</p>
-						</li>
-						<li>
-							<p class="text darkTheme">IOS <span class="comingSoon">(Coming soon)</span></p>
-						</li>
-						<li>
-							<p class="text darkTheme">Embedded solutions</p>
-						</li>
-					</ul>
-						<a href="./softwareSolutions" class="learnMoreButton button shadow1">
-							<h3 >Learn more</h3>
-							<img src="./img/buttonArrow.png" alt="">
-						</a>
-				</li>
-				<li class="websites darkTheme softwareSolutionSections">
-					<h3 class="subtitle darkTheme">Websites</h3>
-					<img src="./img/websites.png" alt="Websites">
-					<ul>
-						<li>
-							<p class="text darkTheme">Modern designs</p>
-						</li>
-						<li>
-							<p class="text darkTheme">Range of features</p>
-						</li>
-						<li>
-							<p class="text darkTheme">Back-end integration</p>
-						</li>
-						<li>
-							<p class="text darkTheme">E-commerce</p>
-						</li>
-					</ul>
-					<a href="./howItWorks" class="howItWorksButton button shadow1">
-						<h3>How it works</h3>
-						<img src="./img/buttonArrow.png" alt="">
-					</a>
-				</li>
-				<li class="systemsDevelopment darkTheme softwareSolutionSections">
-					<h3 class="subtitle darkTheme">Systems Development</h3>
-					<img src="./img/systemsDevelopment.png" alt="Systems Development">
-					<ul>
-						<li>
-							<p class="text darkTheme">Server infrastructure</p>
-						</li>
-						<li>
-							<p class="text darkTheme">Database integration</p>
-						</li>
-						<li>
-							<p class="text darkTheme">Storage systems</p>
-						</li>
-						<li>
-							<p class="text darkTheme">Custom solutions</p>
-						</li>
-					</ul>
-					<a href="./contact" class="contactButton button shadow1">
+		</section>
+		<section class="softwareSolutions" id="development">
+			<div class="softwareSolutionsContainer container">
+				<div class="heading darkTheme">
+					<span><img src="img/headingMarker.png" alt="Heading"></span><h1 class="">Design. Develop. Deploy.</h1>
+				</div>
+				<p class="text darkTheme">Tailored solutions to suit your business. No matter the size.</p>
+				<ul>
+					<li class="customApplications softwareSolutionSections">
+						<h3 class="subtitle darkTheme">Custom Applications</h3>
+						<img src="./img/customApplications.png" alt="Custom Applications">
+						<ul>
+							<li class="text darkTheme">Windows</li>
+							<li class="text darkTheme">Android</li>
+							<li class="text darkTheme">IOS <span class="comingSoon">(Coming soon)</span></li>
+							<li class="text darkTheme">Embedded solutions</li>
+						</ul>
+					</li>
+					<li class="websites darkTheme softwareSolutionSections">
+						<h3 class="subtitle darkTheme">Websites</h3>
+						<img src="./img/websites.png" alt="Websites">
+						<ul>
+							<li class="text darkTheme">Modern designs</li>
+							<li class="text darkTheme">Range of features</li>
+							<li class="text darkTheme">Back-end integration</li>
+							<li class="text darkTheme">E-commerce</li>
+						</ul>
+					</li>
+					<li class="systemsDevelopment darkTheme softwareSolutionSections">
+						<h3 class="subtitle darkTheme">Systems Development</h3>
+						<img src="./img/systemsDevelopment.png" alt="Systems Development">
+						<ul>
+							<li class="text darkTheme">Server infrastructure</li>
+							<li class="text darkTheme">Database integration</li>
+							<li class="text darkTheme">Storage systems</li>
+							<li class="text darkTheme">Custom solutions</li>
+						</ul>
+					</li>
+				</ul>
+					<a href="#contact" class="howItWorksButton darkTheme button shadow1">
 						<h3>Contact</h3>
 						<img src="./img/buttonArrow.png" alt="">
 					</a>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<div class="portfolio">
+			</div>
+		</section>
+	</main>
+	<section class="portfolio" id="portfolio">
 		<div class="portfolioContainer container">
 			<div class="heading">
 				<span><img src="img/headingMarker.png" alt=""></span><h1 class="">Portfolio</h1>
@@ -175,10 +132,10 @@
 					<h3 class="subtitle">Millies Garden</h3>
 					<p class="text">A plant gallery and order page for succulent arrangement</p>
 				</a>
-				<a class = "project shadow2 hidden" href="markblashki.com/portfolio/">
-					<img src="./img/.jpg" alt=" Website" class="shadow1">
-					<h3 class="subtitle"></h3>
-					<p class="text"></p>
+				<a class = "project shadow2" href="http://markblashki.com/portfolio/victoriahouse">
+					<img src="./img/VictoriaHouseHero.png" alt=" Website" class="shadow1">
+					<h3 class="subtitle">Victoria House</h3>
+					<p class="text">Victorian style accommodation on the entrance of the Yarra Valley</p>
 				</a>
 				<a class = "project shadow2 hidden" href="markblashki.com/portfolio/">
 					<img src="./img/.jpg" alt=" Website" class="shadow1">
@@ -187,8 +144,8 @@
 				</a>
 			</div>
 		</div>
-	</div>
-	<div class="contact">
+	</section>
+	<section class="contact" id="contact">
 		<div class="contactContainer container">
 		<p class="text darkTheme">For more details on how Mark Blashki Solutions can help your business or for general enquiries call or email at:</p>
 		<div class="contactDetailsContainer">
@@ -205,6 +162,6 @@
 				<h3 class="subtitle darkTheme"></h3>
 			</div>
 		</div>
-	</div>
+	</section>
 </body>
 </html>
